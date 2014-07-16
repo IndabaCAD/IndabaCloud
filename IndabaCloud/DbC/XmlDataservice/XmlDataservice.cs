99ed060e7959f89cde3aa80250053cbfb153aaea
chasingbob <dries139@gmail.com>
2014-07-16 12:15:44 +0200

    DbC skeleton check-in

---
+++ IndabaCloud/DbC/XmlDataservice/XmlDataservice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace XmlDataservice
+{
+    public class XmlDataservice : IDataservice
+    {
+        public ICollection<Person> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
